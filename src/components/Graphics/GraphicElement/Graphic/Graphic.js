b83661a9722ef50994f49f26c13facd3442bfbd6
Dmytro Markuntovych <sunsn4ke@gmail.com>
2023-02-14 15:55:02 +0200

    feat: add different providers colors to graphics

--- src/components/Graphics/GraphicElement/Graphic/Graphic.js
+++ src/components/Graphics/GraphicElement/Graphic/Graphic.js
@@ -6,13 +6,13 @@ import "./Graphic.scss";
 function Graphic({ providerName }) {
   const data = useSelector((state) => state.prices[providerName]);
   const totalPrice = data.totalPrice;
-  const isLowerPrice = data.lowestPrice;
+  const isLowerPrice = data.lowestPrice ? `graphic_line_top__${providerName}` : '';
   const isPortrait = useMediaQuery({ query: "(orientation: portrait)" });
   const style = isPortrait ? {"height": `${totalPrice + 5}%`} : {"width": `${totalPrice + 5}%`};
 
   return (
     <div className="graphic">
-      <div className={`graphic_line ${isLowerPrice && "graphic_line_top"}`} style={style} />
+      <div className={`graphic_line ${isLowerPrice}`} style={style} />
       <div className="graphic_price">{totalPrice} $</div>
     </div>
   );
